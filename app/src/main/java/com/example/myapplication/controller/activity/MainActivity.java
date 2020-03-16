72800089ef1590cc514b6ee929c85986e629fb8d
SergioGarciaLS <sergio.garcia@students.salle.url.edu>
2020-03-16 15:42:46 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/controller/activity/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.controller.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bLogin;
+    private Button bRegister;
+    private TextView tvRecordarContra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        etUsername = (EditText) findViewById(R.id.main_username);
+        etPassword = (EditText) findViewById(R.id.main_password);
+        bLogin = (Button) findViewById(R.id.main_logIn_button);
+        bRegister = (Button) findViewById(R.id.main_register_button);
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+        tvRecordarContra = (TextView) findViewById(R.id.main_recordar_contra);
+    }
+}
