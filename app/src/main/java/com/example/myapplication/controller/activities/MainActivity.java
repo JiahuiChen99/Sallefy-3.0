b713b8940a813559dd97eaf76731a1dc3d0c5189
SergioGarciaLS <sergio.garcia@students.salle.url.edu>
2020-03-17 15:23:00 +0100

    Barra menu completa

--- app/src/main/java/com/example/myapplication/controller/activities/MainActivity.java
+++ app/src/main/java/com/example/myapplication/controller/activities/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private Button bRegister;
     private TextView tvRecordarContra;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
