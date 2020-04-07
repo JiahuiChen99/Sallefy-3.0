dde5175d5cddfa5d363195d774867f11b91d66f3
MasterofDragons <Jiahui1@hotmail.es>
2020-04-07 14:01:25 +0200

    feat: Track REST API calls

--- app/src/main/java/com/example/myapplication/restapi/service/TrackService.java
+++ app/src/main/java/com/example/myapplication/restapi/service/TrackService.java
@@ -6,8 +6,10 @@ import com.example.myapplication.model.Track;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface TrackService {
@@ -17,6 +19,18 @@ public interface TrackService {
 
     @GET("tracks/{id}")
     Call<Track> getSpecificTrack(@Path("id") int id, @Header("Authorization") String token);
+
+    @GET("tracks/{id}/like")
+    Call<Track> likedTrack(@Path("id") int id, @Header("Authorization") String token);
+
+    @PUT("tracks/{id}/like")
+    Call<Track> likeTrack(@Path("id") int id, @Header("Authorization") String token);
+
+    @PUT("tracks/{id}/play")
+    Call<Track> playTrack(@Path("id") int id, @Header("Authorization") String token);
+
+    @DELETE
+    Call<Track> deleteTrack(@Path("id") int id, @Header("Authorization") String token);
 
     @GET("tracks?recent=true&size=10")
     Call<List<Track>> getRecentTracks(@Header("Authorization") String token);
