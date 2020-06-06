01933fa6b8dacae67e2399710d0845e778ab0527
Gonzalo Yubero Garcia <gonzalo.yubero@students.salle.url.edu>
2020-06-06 13:10:28 +0200

    #1 feat: follow class has now atributes

--- app/src/main/java/com/example/myapplication/model/Follow.java
+++ app/src/main/java/com/example/myapplication/model/Follow.java
@@ -1,4 +1,18 @@
 package com.example.myapplication.model;
 
 public class Follow {
+
+    private String followed;
+
+    public Follow(String followed) {
+        this.followed = followed;
+    }
+
+    public String getFollowed() {
+        return followed;
+    }
+
+    public void setFollowed(String followed) {
+        this.followed = followed;
+    }
 }
